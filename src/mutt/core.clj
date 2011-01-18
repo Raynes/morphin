137ca27adb36d1576c30c7bebc0d4545c5eefc38
Alan Malloy <alan@malloys.org>
2011-01-17 22:13:16 -0800

    Handle negative steps and empty ranges (incl significant reorg to make that work)

--- src/mutt/core.clj
+++ src/mutt/core.clj
@@ -1,28 +1,30 @@
 (ns mutt.core)
 
+(defn- sign [x]
+  (cond
+   (neg? x) -1
+   (pos? x) 1
+   :else 0))
+
 (defprotocol Rangeable
   (first-item [_])
-  (after [item])
-  (next-item [prev end step]))
+  (after? [a b])
+  (next-item [prev step]))
 
 (extend-protocol Rangeable
   Number
   (first-item [_] 0)
-  (after [num] (inc num))
+  (after? [a b] (>= a b))
   (next-item
-   ([prev end step]
-      (let [res (+ prev step)]
-        (when (< res end)
-          res))))
+   ([prev step]
+      (+ prev step)))
   
   Character
   (first-item [_] Character/MIN_VALUE)
-  (after [c] (char (inc (int c))))
+  (after? [a b] (>= (int a) (int b)))
   (next-item
-   ([prev end step]
-      (let [res (+ (int step) (int prev))]
-        (when (< res (int end))
-          (char res))))))
+   ([prev step]
+      (char (+ (int step) (int prev))))))
 
 (defn range
   ([] (range 0 Double/POSITIVE_INFINITY 1 true))
@@ -30,10 +32,14 @@
   ([start end] (range start end 1 true))
   ([start end step] (range start end step true))
   ([start end step exc?]
-     (let [end (if exc? end (after end))]
+     (let [s (sign step)
+           end (if exc? end (next-item end s))
+           after? (if (= 1 s)
+                   after?
+                   #(after? %2 %1))]
        ((fn range* [item]
           (lazy-seq
-           (cons item
-                 (when-let [res (next-item item end step)]
-                   (range* res)))))
+           (when-not (after? item end)
+             (cons item
+                   (range* (next-item item step))))))
         start))))
