2f43d9d839b1bbe01128ce0943d63506e9e612ea
Alan Malloy <alan@malloys.org>
2011-01-17 22:01:45 -0800

    Basic mock-up: doesn't work for negative steps or empty ranges

--- src/mutt/core.clj
+++ src/mutt/core.clj
@@ -1,31 +1,39 @@
 (ns mutt.core)
 
 (defprotocol Rangeable
-  (prange [end exc?] [start end exc?] [start end step exc?]))
+  (first-item [_])
+  (after [item])
+  (next-item [prev end step]))
 
-(extend-type Number
-  Rangeable
-  (prange
-   ([end exc?] (prange 0 end 1 exc?))
-   ([start end exc?] (prange start end 1 exc?))
-   ([start end step exc?]
-      (lazy-seq
-       (let [comparison (if (pos? step) >= <=)]
-         (when-not (comparison start (if exc? end (inc end)))
-           (let [inc-fn (if-not (= step 0) (partial + step) inc)]
-             (cons start (prange (inc-fn start) end step exc?)))))))))
-
-(extend-type Character
-  Rangeable
-  (prange
-   ([end exc?] (prange (Character/MIN_VALUE) end 1 exc?))
-   ([start end exc?] (prange start end 1 exc?))
-   ([start end step exc?]
-      (map char (prange (int start) (int end) step exc?)))))
+(extend-protocol Rangeable
+  Number
+  (first-item [_] 0)
+  (after [num] (inc num))
+  (next-item
+   ([prev end step]
+      (let [res (+ prev step)]
+        (when (< res end)
+          res))))
+  
+  Character
+  (first-item [_] Character/MIN_VALUE)
+  (after [c] (char (inc (int c))))
+  (next-item
+   ([prev end step]
+      (let [res (+ (int step) (int prev))]
+        (when (< res (int end))
+          (char res))))))
 
 (defn range
-  ([] (prange 0 (Double/POSITIVE_INFINITY) 1 true))
-  ([end] (prange end true))
-  ([start end] (prange start end 1 true))
-  ([start end step] (prange start end step true))
-  ([start end step exclusive?] (prange start end step exclusive?)))
+  ([] (range 0 Double/POSITIVE_INFINITY 1 true))
+  ([end] (range (first-item end) end 1 true))
+  ([start end] (range start end 1 true))
+  ([start end step] (range start end step true))
+  ([start end step exc?]
+     (let [end (if exc? end (after end))]
+       ((fn range* [item]
+          (lazy-seq
+           (cons item
+                 (when-let [res (next-item item end step)]
+                   (range* res)))))
+        start))))
